21af7affe0f0f318e3d38c024b3fee768738ffd7
yoong9627 <yoong9627@gmail.com>
2017-09-29 09:09:32 -0700

    fix

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -6,7 +6,7 @@ Rails.application.config.middleware.use OmniAuth::Builder do
            hd: 'g.ucla.edu',
 
           client_options: {ssl:
-            {ca_file: Rails.root.join("cacert.pem").to_s}
+            {ca_file: Rails.root.join('lib/assets/cacert.pem').to_s}
             }
         }
 end

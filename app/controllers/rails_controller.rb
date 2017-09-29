117f30c7c3a567d353721a164a2df3c375a88697
yoong9627 <yoong9627@gmail.com>
2017-09-28 22:14:34 -0700

    test

--- app/controllers/rails_controller.rb
+++ app/controllers/rails_controller.rb
@@ -1,6 +1,6 @@
-class RailsController < ApplicationController
-def db
-  unless current_user.tier == 1
-    redirect_to root_path
-  end
-end
+# class RailsController < ApplicationController
+# def db
+#   unless current_user.tier == 1
+#     redirect_to root_path
+#   end
+# end

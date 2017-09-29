e3391f9239e67cc5982d0e9eb33a9eec5ce3dcb0
yoong9627 <yoong9627@gmail.com>
2017-09-29 09:40:15 -0700

    glogin fix

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,7 +1,7 @@
 OmniAuth.config.logger = Rails.logger
 
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :google_oauth2, '318485249752-vi8snnhkrdktcbmtfi728q680377bs4b.apps.googleusercontent.com', 'reekNs4nsdF6E--1jId7yu6V,
+  provider :google_oauth2, '318485249752-vi8snnhkrdktcbmtfi728q680377bs4b.apps.googleusercontent.com', 'reekNs4nsdF6E--1jId7yu6V',
         {
            hd: 'g.ucla.edu',
 

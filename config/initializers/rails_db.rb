0e5f7895074db07a74628a235972ee43ccd254d2
yoong9627 <yoong9627@gmail.com>
2017-09-28 23:07:43 -0700

    admin

--- config/initializers/rails_db.rb
+++ config/initializers/rails_db.rb
@@ -19,10 +19,10 @@ if Object.const_defined?('RailsDb')
     # config.http_basic_authentication_user_name = 'rails_db'
 
     # # Enable http basic authentication
-    # config.http_basic_authentication_password = 'password'
+    config.http_basic_authentication_password = 'brueut170928'
 
     # # Enable http basic authentication
     # config.verify_access_proc = proc { |controller| true }
-    config.verify_access_proc = proc { |controller| (controller.current_user.tier==1) }
+    # config.verify_access_proc = proc { |controller| (controller.current_user.tier==1) }
   end
 end

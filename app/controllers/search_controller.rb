62cb5a9b5b69c04e495f3e0c5e696e13e3529263
hyunjerry <hyunjerry@hotmail.com>
2017-10-03 20:44:37 -0700

    update event - ksu, readymade

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -4,30 +4,30 @@ class SearchController < ApplicationController
         term = params[:term].strip
         term = term.gsub("  ", " ").gsub(" ", "%")
 
-        # search_party = Partypost.where("article LIKE ?", "%#{term}%")
-        # # party_user = Partypost.where("user LIKE ?" , "%#{term}%")
-        # # partycomment = Partycomment.where("content LIKE ?" , "%#{term}%")
-        # # partycomment_user = Partycomment.where("user LIKE ?" , "%#{term}%")
-        # # partylike = Partylike.where("user LIKE ?" , "%#{term}%")
-        # # partyjoin = Partyjoin.where("user LIKE ?" , "%#{term}%")
-        #
-        # # @party = party + party_user + partycomment + partycomment_user + partylike + partycomment
-        # @partys = search_party
-        # @partys = @partys.uniq
-        # @partys = @partys.flatten
+        search_party = Partypost.where("article LIKE ?", "%#{term}%")
+        party_user = Partypost.where("user LIKE ?" , "%#{term}%")
+        partycomment = Partycomment.where("content LIKE ?" , "%#{term}%")
+        partycomment_user = Partycomment.where("user LIKE ?" , "%#{term}%")
+        partylike = Partylike.where("user LIKE ?" , "%#{term}%")
+        partyjoin = Partyjoin.where("user LIKE ?" , "%#{term}%")
+
+        @party = party + party_user + partycomment + partycomment_user + partylike + partycomment
+        @partys = search_party
+        @partys = @partys.uniq
+        @partys = @partys.flatten
 
 
 
 
 
-        # search_title = Pointless.where("title LIKE ?", "%#{term}%")
-        # search_content = Pointless.where("content LIKE ?" , "%#{term}%")
-        #
-        #
-        # @pointless = search_title + search_content
-        #         @pointless = @pointless.uniq
-        #
-        #         @pointless = @pointless.flatten
+        search_title = Pointless.where("title LIKE ?", "%#{term}%")
+        search_content = Pointless.where("content LIKE ?" , "%#{term}%")
+
+
+        @pointless = search_title + search_content
+                @pointless = @pointless.uniq
+
+                @pointless = @pointless.flatten
 
 
 

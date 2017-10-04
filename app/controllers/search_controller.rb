77edefe9ead2c6141d862788e3202c16f958b0a7
hyunjerry <hyunjerry@hotmail.com>
2017-10-03 20:43:26 -0700

    event update - ksu, readymade

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -4,32 +4,13 @@ class SearchController < ApplicationController
         term = params[:term].strip
         term = term.gsub("  ", " ").gsub(" ", "%")
 
-        # search_party = Partypost.where("article LIKE ?", "%#{term}%")
-        # # party_user = Partypost.where("user LIKE ?" , "%#{term}%")
-        # # partycomment = Partycomment.where("content LIKE ?" , "%#{term}%")
-        # # partycomment_user = Partycomment.where("user LIKE ?" , "%#{term}%")
-        # # partylike = Partylike.where("user LIKE ?" , "%#{term}%")
-        # # partyjoin = Partyjoin.where("user LIKE ?" , "%#{term}%")
-        #
-        # # @party = party + party_user + partycomment + partycomment_user + partylike + partycomment
-        # @partys = search_party
-        # @partys = @partys.uniq
-        # @partys = @partys.flatten
+        search_party = Partypost.where("article LIKE ?", "%#{term}%")
+        partycomment = Partycomment.where("content LIKE ?" , "%#{term}%")
 
 
-
-
-
-        # search_title = Pointless.where("title LIKE ?", "%#{term}%")
-        # search_content = Pointless.where("content LIKE ?" , "%#{term}%")
-        #
-        #
-        # @pointless = search_title + search_content
-        #         @pointless = @pointless.uniq
-        #
-        #         @pointless = @pointless.flatten
-
-
+        @partyposts = search_party + partycomment
+        @partyposts = @partyposts.uniq
+        @partyposts = @partyposts.flatten
 
     end
 end
--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,7 +1,7 @@
 OmniAuth.config.logger = Rails.logger
 
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :google_oauth2, '318485249752-jfr8l5j12476t3u54s2gsv5t862kvv91.apps.googleusercontent.com', 'mCXPjz1N3AC0x9z3CxvluTaT',
+  provider :google_oauth2, '318485249752-vi8snnhkrdktcbmtfi728q680377bs4b.apps.googleusercontent.com', 'reekNs4nsdF6E--1jId7yu6V',
         {
            hd: 'g.ucla.edu',
 
